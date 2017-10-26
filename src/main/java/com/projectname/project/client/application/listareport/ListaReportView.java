1ad88e8b6e684354a89a9c3dad4a8030f49e68c0
David Bertini <david.bertini@gmail.com>
2017-10-26 17:06:20 +0200

    Aggiunto bootstrao

--- src/main/java/com/projectname/project/client/application/listareport/ListaReportView.java
+++ src/main/java/com/projectname/project/client/application/listareport/ListaReportView.java
@@ -2,16 +2,38 @@ package com.projectname.project.client.application.listareport;
 
 import javax.inject.Inject;
 
+import org.gwtbootstrap3.client.ui.Button;
+
+import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.ui.Widget;
 import com.gwtplatform.mvp.client.ViewImpl;
+import com.google.gwt.user.client.Timer;
 
 public class ListaReportView extends ViewImpl implements ListaReportPresenter.MyView {
+	
+	@UiField
+	Button bottone;
+	
 	interface Binder extends UiBinder<Widget, ListaReportView> {
 	}
 
 	@Inject
 	ListaReportView(Binder uiBinder) {
 		initWidget(uiBinder.createAndBindUi(this));
+	}
+	
+	@UiHandler("bottone")
+	public void onButtonClick(final ClickEvent event) {
+		bottone.state().loading();
+
+        new Timer() {
+            @Override
+            public void run() {
+            	bottone.state().reset();
+            }
+        }.schedule(2000);
 	}
 }
